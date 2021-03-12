b0c8235b82e72ad040c91c4110de7ef18357d3ab
jnsereko <admin@jnsereko.com>
2021-03-12 13:52:38 +0300

    TRUNK-5466: Added logic for location to support Visits

--- api/src/main/java/org/openmrs/Location.java
+++ api/src/main/java/org/openmrs/Location.java
@@ -81,6 +81,8 @@ public class Location extends BaseCustomizableMetadata<LocationAttribute> implem
 
 	private Location parentLocation;
 	
+	private Boolean supportsVisits;
+	
 	private Set<Location> childLocations;
 	
 	@Independent
@@ -773,5 +775,21 @@ public class Location extends BaseCustomizableMetadata<LocationAttribute> implem
 	@Override
 	public void setAddress15(String address15) {
 		this.address15 = address15;
+	}
+	
+	/**
+	 * @return the supportsVisits
+	 * @since 2.4.0
+	 */
+	public Boolean getSupportsVisits() {
+		return supportsVisits;
+	}
+	
+	/**
+	 * @param supportsVisits the supportsVisits to set
+	 * @since 2.4.0
+	 */
+	public void setSupportsVisits(Boolean supportsVisits) {
+		this.supportsVisits = supportsVisits;
 	}
 }
